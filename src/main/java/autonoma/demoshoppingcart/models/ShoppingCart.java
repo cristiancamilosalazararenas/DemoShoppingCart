4fa6e92f4b5b18215a8d16faa533da211916541d
Juan Sebastian Lopez Guzman <juanse150906@gmail.com>
2025-05-14 17:47:35 -0500

    Se agregaron los metodos de la clase ShoppingCart, solo faktaria el buy()

--- src/main/java/autonoma/demoshoppingcart/models/ShoppingCart.java
+++ src/main/java/autonoma/demoshoppingcart/models/ShoppingCart.java
@@ -4,10 +4,44 @@
  */
 package autonoma.demoshoppingcart.models;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Asus
  */
 public class ShoppingCart {
+    private int id=1;
+    private ArrayList<Product> product;
+
+    public ShoppingCart() {
+    }
+    public boolean addProduct(Product product){
+        this.product.add(product);
+        return true;
+    }
+    public boolean remoteProduct(int num){
+        if (this.product.size()<=num){
+            this.product.remove(num);
+            return true;
+        }
+        else{
+            return false;
+        }
+    }   
+    public Product serchProduct(int num){
+        for (int i=0; i<this.product.size();i++){
+            if (i == num){
+                return this.product.get(i);
+            }
+        }
+        return null;
+    }
+    public int size(){
+        return this.product.size();
+    }
+    public void buy(){
+        
+    }
     
 }
